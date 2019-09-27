f3ba77b07200a8f4175e249b1e11b4ac8c996794
Haewon Shin <hshin@Haewonui-MacBookPro.local>
2019-09-27 13:07:01 +0900

    first commit

--- quotes-service/src/main/java/io/pivotal/quotes/domain/IexQuote.java
+++ quotes-service/src/main/java/io/pivotal/quotes/domain/IexQuote.java
@@ -12,20 +12,47 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class IexQuote {
-
     private String symbol;
     private String companyName;
+    private String primaryExchange;
+    private String calculationPrice;
     private BigDecimal open;
+    private Long openTime;
     private BigDecimal close;
     private Long closeTime;
     private BigDecimal high;
     private BigDecimal low;
     private BigDecimal latestPrice;
-    private Long latestUpdate;
     private String latestSource;
+    private String latestTime;
+    private Long latestUpdate;
+    private BigDecimal latestVolume;
+    private BigDecimal iexRealtimePrice;
+    private String iexRealtimeSize;
+    private String iexLastUpdated;
+    private BigDecimal delayedPrice;
+    private Long delayedPriceTime;
+    private String extendedPrice;
+    private String extendedChange;
+    private BigDecimal extendedChangePercent;
+    private Long extendedPriceTime;
+    private String previousClose;
+    private BigDecimal previousVolume;
     private BigDecimal change;
     private BigDecimal changePercent;
+    private String volume;
+    private BigDecimal iexMarketPercent;
+    private String iexVolume;
     private BigDecimal avgTotalVolume;
+    private BigDecimal iexBidPrice;
+    private String iexBidSize;
+    private BigDecimal iexAskPrice;
+    private String iexAskSize;
     private BigDecimal marketCap;
-
+    private String peRatio;
+    private BigDecimal week52High;
+    private BigDecimal week52Low;
+    private BigDecimal ytdChange;
+    private Long lastTradeTime;
+    private String isUSMarketOpen;
 }
